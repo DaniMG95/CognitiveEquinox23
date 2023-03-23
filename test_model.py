8a8a9cbd4b74c16b5b81d138cbf2ce944e372e2c
Javier Lasheras <javierlasheras.pena@telefonica.com>
2023-03-23 16:56:44 +0100

    Add repository of Songs

---
+++ test_model.py
@@ -0,0 +1,14 @@
+from musicvec.model import Song, SongRepository
+from qdrant_client import QdrantClient
+
+song = Song(
+    id=1,
+    artist_name="Los Gandules",
+    track_name="Cleptocupromano",
+    lyrics="lkajsdf lkasdjfh lkajfdhlkj aldksfj hlaksdjfh lafdjkh asdfasd"
+)
+
+repo = SongRepository(client=QdrantClient())
+
+repo.create_collection(force=True)
+repo.add_songs([song])
