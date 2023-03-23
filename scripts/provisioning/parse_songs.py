eb0afeabab3696e8a623e6a9ae2672ce8d0da4b2
Pedro Guillen <pedro.guillenaroca@telefonica.com>
2023-03-23 16:54:31 +0100

    refactor parser

--- scripts/provisioning/parse_songs.py
+++ scripts/provisioning/parse_songs.py
@@ -1,17 +1,39 @@
 import csv
+import datetime
+from scripts.datatovector import DataToVector
 
-with open('../../resources/tcc_ceds_music.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    first_row = True
-    for row in csv_reader:
-        if not first_row:
-            artist_name = row[1]
-            track_name = row[2]
-            release_date = row[3]
-            genre = row[4]
-            lyrics = row[5]
-            song_len = row[6]
-            # get vector
-            # save to database
-            print(lyrics)
-        first_row = False
+
+def parse_data():
+    data_to_vector = DataToVector()
+
+    with open(f'../../resources/vectorize_music{int(datetime.datetime.now().timestamp())}.csv', mode='w') as output_csv_file:
+        with open('../../resources/tcc_ceds_music.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            csv_writer = csv.writer(output_csv_file, delimiter=';')
+            counter = 0
+
+            for row in csv_reader:
+                if counter == 0:
+                    csv_writer.writerow(['artist_name', 'track_name', 'release_date', 'genre', 'lyrics', 'song_len',
+                                         'topic', 'age', 'lyrics_vector'])
+                else:
+                    artist_name = row[1]
+                    track_name = row[2]
+                    release_date = row[3]
+                    genre = row[4]
+                    lyrics = row[5]
+                    song_len = row[6]
+                    topic = row[-2]
+                    age = row[-1]
+                    lyrics_vector = data_to_vector.prepare_input(input_string=lyrics)
+
+                    csv_writer.writerow([artist_name, track_name, release_date, genre, lyrics, song_len, topic, age,
+                                         lyrics_vector.tolist()])
+
+                if counter == 10:
+                    break
+                counter = counter + 1
+
+
+if __name__ == '__main__':
+    parse_data()
